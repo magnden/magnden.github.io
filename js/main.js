b00d95bd4b6e2780b765ae8a24f7a372ed4363f3
Dennis Magnusson <dennis@Dennis-MBP.home>
2018-03-23 07:26:17 +0200

    mobile menu added

--- js/main.js
+++ js/main.js
@@ -1,49 +1,79 @@
-
-
+// Dennis Magnusson 2018 (c)
 
 $(document).ready(function(){
 
-  setInterval(function(){
-    animateArrow();
-  }, 3000);
+  var wrapperMenu = $('.wrapper-menu');
+  var navMenu = $('.navMenu');
 
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 180) {
-	    $("#topHeader").css("box-shadow" , "0px 4px 51px #ccc");
-	  }
+  wrapperMenu.click(function(){
+    wrapperMenu.toggleClass('open');
+    // navMenu.toggleClass('showing');
+    if ( navMenu.is( ":hidden" ) ) {
+    navMenu.slideDown()
+  } else {
+    navMenu.slideUp();
+  }
 
-	  else{
-		  $("#topHeader").css("box-shadow" , "none");
-	  }
-  })
-
-  // Scroll Animations
-  $('#arrowDown').on('click', scrollToPortfolio)
-  $('#toPortfolio').on('click', scrollToPortfolio)
-  $('#toAbout').on('click', scrollToAbout)
-  $('#toContact').on('click', scrollToContact)
-
-  $('#submit-form').on('click', function(){
-    var nameField = $('#name-input');
-    var emailField = $('#email-input');
-    var messageField = $('#message-field');
-    if(nameField.val || emailField.val() || messageField.val()){
-
-      if (emailField.val().indexOf('@') > -1 || emailFieldval().indexOf('.') > -1) {
-        $('footer').css('background-color', 'green')
-      }
-      else {
-        $('footer').css('background-color', 'red')
-      }
-
-    } else {
-      $('footer').css('background-color', 'red')
-    }
   });
+
+  var is_mobile = false;
 
 
 });
+
+$( window ).resize(function() {
+  // Check if client is mobile
+  if( $('.navMenu').css('display')=='flex') {
+      is_mobile = true;
+  }
+
+  if (is_mobile == true) {
+    
+    navMenu.hide();
+
+    // Jumping Arrow
+    setInterval(function(){
+      animateArrow();
+    }, 3000);
+
+    // Scroll Animations
+    $('#arrowDown').on('click', scrollToPortfolio)
+    $('#toPortfolio').on('click', scrollToPortfolio)
+    $('#toAbout').on('click', scrollToAbout)
+    $('#toContact').on('click', scrollToContact)
+
+    $('#submit-form').on('click', function(){
+      var nameField = $('#name-input');
+      var emailField = $('#email-input');
+      var messageField = $('#message-field');
+      if(nameField.val || emailField.val() || messageField.val()){
+
+        if (emailField.val().indexOf('@') > -1 || emailFieldval().indexOf('.') > -1) {
+          $('footer').css('background-color', 'green')
+        }
+        else {
+          $('footer').css('background-color', 'red')
+        }
+
+      } else {
+        $('footer').css('background-color', 'red')
+      }
+    });
+
+
+  };
+});
+
+$(window).scroll(function(){
+  var scroll = $(window).scrollTop();
+  if (scroll > 180) {
+    $("#topHeader").css("box-shadow" , "0px 4px 51px #ccc");
+  }
+
+  else{
+    $("#topHeader").css("box-shadow" , "none");
+  }
+})
 
 function scrollToPortfolio(){
   $('html, body').animate({
